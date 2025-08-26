993d4f4dd1dfd759c605ab30f84bafc0cb995649
Łukasz Sobczak <81999946+sobczal2@users.noreply.github.com>
2025-08-26 18:08:13 +0200

    update neovim config

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -4,7 +4,7 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     cmd = function()
         local builtin = require 'telescope.builtin'
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles' })
-        vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = '[G]it [F]iles' })
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles', noremap = true, silent = true })
+        vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = '[G]it [F]iles', noremap = true, silent = true })
     end,
 }
